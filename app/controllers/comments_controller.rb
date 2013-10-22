4a15d37e33e983c14d5eea68c177acba1390d6e0
Adam Raouki <adam0964@gmail.com>
2013-10-22 11:38:05 -0400

    Add DISQUS for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
 
-  before_filter :authenticate_user!, :except => [:show, :index]
+  # before_filter :authenticate_user!, :except => [:show, :index]
 
 	def create
     @post = Post.find(params[:post_id])
